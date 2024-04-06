6bb55970b5f5820cf6382ba2467496518be1ab4e
Tao242dn <tao242dn@gmail.com>
2024-04-06 19:40:36 +0700

    Updated Item.jsx

--- front-end/src/pages/item/Item.jsx
+++ front-end/src/pages/item/Item.jsx
@@ -3,17 +3,17 @@ import './item.css'
 import creator from '../../assets/seller2.png'
 import { useParams } from 'react-router-dom';
 import { useWallet } from '@suiet/wallet-kit'
-//import { TransactionBlock } from "@mysten/sui.js";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
 
 const mapping = {
-  0: { targetId: "0", name: "Abstract Smoke Red", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids1.png?raw=true" },
-  1: { targetId: "1", name: "Mountain Landscape", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids2.png?raw=true" },
-  2: { targetId: "2", name: "Paint Colour on Wall", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids3.png?raw=true" },
-  3: { targetId: "3", name: "Abstract Pattern", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids4.png?raw=true" },
-  4: { targetId: "4", name: "White Line Grafiti", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids5.png?raw=true" },
-  5: { targetId: "5", name: "Abstract Triangle", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids6.png?raw=true" },
-  6: { targetId: "6", name: "Lake Landscape", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids7.png?raw=true" },
-  7: { targetId: "7", name: "Blue Red Art", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids8.png?raw=true" },
+  0: {address: "", targetId: "0", name: "Abstract Smoke Red", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids1.png?raw=true", price: 0 },
+  1: {address: "", targetId: "1", name: "Mountain Landscape", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids2.png?raw=true", price: 0 },
+  2: {address: "", targetId: "2", name: "Paint Colour on Wall", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids3.png?raw=true", price: 0 },
+  3: {address: "", targetId: "3", name: "Abstract Pattern", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids4.png?raw=true", price: 0 },
+  4: {address: "", targetId: "4", name: "White Line Grafiti", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids5.png?raw=true", price: 0 },
+  5: {address: "", targetId: "5", name: "Abstract Triangle", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids6.png?raw=true", price: 0 },
+  6: {address: "", targetId: "6", name: "Lake Landscape", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids7.png?raw=true", price: 0 },
+  7: {address: "", targetId: "7", name: "Blue Red Art", imgAddress: "https://github.com/kasim393/NFT-Marketplace-UI/blob/main/src/assets/bids8.png?raw=true", price: 0 },
 };
 const Item = () => {
   let id = useParams().id;
@@ -35,14 +35,22 @@ const Item = () => {
   const handleOnClick = () => {
     let targetId = mapping[id].targetId;
     let address = wallet.account?wallet.account.address:"";
+    let price = mapping[id].price;
     console.log("buying", targetId, address);
 
-    // let tx = new TransactionBlock();
+    let tx = new TransactionBlock();
     // let kioskArg = tx.object('<ID>');
-    // let capArg = tx.object('<ID>');
-    // let itemId = tx.pure.address('<ID>');
+    let capArg = tx.object(`${address}`);
+    let itemId = tx.pure.address(`${targetId}`);
+    let priceArg = tx.pure.u64(`${price}`);
+    
     // let itemType = 'ITEM_TYPE';
     //delist(tx, itemType, kioskArg, capArg, itemId);
+
+    tx.moveCall({
+      target: '0x2::kiosk::list',
+      arguments: [capArg, itemId, priceArg]
+    })
 
     disableButton();
     return;
